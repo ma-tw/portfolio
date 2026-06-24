a0206f0be2b5c0d28e28952bff357e81406d10b3
ma_tw <ma_tw@example.com>
2026-06-24 20:20:03 +0900

    Publish?

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,9 +1,10 @@
 import type { GatsbyConfig } from "gatsby";
 
 const config: GatsbyConfig = {
+  pathPrefix: "/portfolio",
   siteMetadata: {
-    title: `Portfolio`,
-    siteUrl: `https://www.yourdomain.tld`
+    title: `mahta's Portfolio`,
+    siteUrl: `https://ma-tw.github.io/portfolio/`
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
